970e18563eb9e3b6815d4b2db03163a72cbeccdb
Mahmoud1Ali3ng <59738792+MahmoudAliEng@users.noreply.github.com>
2020-01-13 23:23:00 +0100

    Defining the network architecture

--- MNIST_MLP.py
+++ MNIST_MLP.py
@@ -41,3 +41,60 @@ for idx in np.arange(20):
     # print out the correct label for each image
     # .item() gets the value contained in a Tensor
     ax.set_title(str(labels[idx].item()))
+    
+# View an Image in More Detail
+img = np.squeeze(images[1])
+
+fig = plt.figure(figsize = (12,12)) 
+ax = fig.add_subplot(111)
+ax.imshow(img, cmap='gray')
+width, height = img.shape
+thresh = img.max()/2.5
+for x in range(width):
+    for y in range(height):
+        val = round(img[x][y],2) if img[x][y] !=0 else 0
+        ax.annotate(str(val), xy=(y,x),
+                    horizontalalignment='center',
+                    verticalalignment='center',
+                    color='white' if img[x][y]<thresh else 'black')    
+
+ 
+# Defining the archetecture of the network
+import torch.nn as nn
+import torch.nn.functional as F
+
+# define the NN architecture
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # number of hidden nodes in each layer (512)
+        hidden_1 = 512
+        hidden_2 = 512
+        # linear layer (784 -> hidden_1)
+        self.fc1 = nn.Linear(28 * 28, hidden_1)
+        # linear layer (n_hidden -> hidden_2)
+        self.fc2 = nn.Linear(hidden_1, hidden_2)
+        # linear layer (n_hidden -> 10)
+        self.fc3 = nn.Linear(hidden_2, 10)
+        # dropout layer (p=0.2)
+        # dropout prevents overfitting of data
+        self.dropout = nn.Dropout(0.2)
+
+    def forward(self, x):
+        # flatten image input
+        x = x.view(-1, 28 * 28)
+        # add hidden layer, with relu activation function
+        x = F.relu(self.fc1(x))
+        # add dropout layer
+        x = self.dropout(x)
+        # add hidden layer, with relu activation function
+        x = F.relu(self.fc2(x))
+        # add dropout layer
+        x = self.dropout(x)
+        # add output layer
+        x = self.fc3(x)
+        return x
+
+# initialize the NN
+model = Net()
+print(model)
