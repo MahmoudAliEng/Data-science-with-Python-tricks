5d2aaf686cfedf0ff2ac5f0f2b869a6eff34eb53
Mahmoud1Ali3ng <59738792+MahmoudAliEng@users.noreply.github.com>
2020-02-10 10:55:36 +0100

    Task 2

--- Interview_preparation.py
+++ Interview_preparation.py
@@ -29,3 +29,21 @@ print(LetterChanges(input()))
 "Argument goes here z"
 Bshvnfou hpft ifsf z
 '''
+
+'''
+Given an array ‘A’ of sorted integers and another non negative integer k, find if there exists 2 indices i and j such that A[i] - A[j] = k, i != j.
+
+A : [1 3 5] 
+k : 4
+
+Return 0 / 1 ( 0 for false, 1 for true ) for this problem
+
+Try doing this in less than linear space complexity.
+'''
+
+def diffPossible(self, A, B):
+	    for i in reversed(range(len(A) )):
+	        for j in range (0, i):
+	            if (A[i] - A[j] == B):
+	                return 1
+        return 0
